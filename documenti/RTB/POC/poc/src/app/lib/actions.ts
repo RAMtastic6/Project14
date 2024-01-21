c0ebf1774bc279d8fba9ac75e5d07bff83f4fe2f
Riccardo <riccardozaupa02@gmail.com>
2024-01-21 17:12:13 +0100

    Create reservation stampa i dati della prenotazione, test funzionante

--- documenti/RTB/POC/poc/src/app/lib/actions.ts
+++ documenti/RTB/POC/poc/src/app/lib/actions.ts
@@ -1,11 +1,11 @@
 'use server';
 
-export async function createReservation(formData: FormData){
-    console.log('dio');
+export async function createReservation(formData: FormData){;
     const rawFormData = {
         restaurantId: formData.get('restaurantId'),
         date: formData.get('date'),
         time: formData.get('time'),
         numberOfPeople: formData.get('numberOfPeople'),
     };
+    console.log(rawFormData);
 }
