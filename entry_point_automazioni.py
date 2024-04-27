f3971192d125c7a7123c64a6607075b9a4ebe1fd
Riccardo <riccardozaupa02@gmail.com>
2024-04-28 00:54:59 +0200

    Inserito Piano di Qualifica v0.2.0 con i riferimenti al glossario

--- entry_point_automazioni.py
+++ entry_point_automazioni.py
@@ -16,5 +16,5 @@ PATH_TO_PDW_FOLDER = os.path.join(os.getcwd(), "sorgenti", "Piano_Di_Qualifica/"
 build_glossario(PATH_TO_GLOSSARY_FOLDER)
 repl_all_occurrences(PATH_TO_GLOSSARY_JSON, "sorgenti/Analisi dei Requisiti/main.tex", "sorgenti/Analisi dei Requisiti/uc/")
 # repl_all_occurrences(PATH_TO_GLOSSARY_JSON, "sorgenti/Piano_Di_Progetto/main.tex", "sorgenti/Piano_Di_Progetto/sections/")
+repl_all_occurrences(PATH_TO_GLOSSARY_JSON, "sorgenti/Piano_Di_Qualifica/main.tex", "sorgenti/Piano_Di_Qualifica/sections/")
 repl_all_occurrences(PATH_TO_GLOSSARY_JSON, "sorgenti/Norme di Progetto/main.tex", "sorgenti/Norme di Progetto/Contents/")
-# repl_all_occurrences(PATH_TO_GLOSSARY_JSON, "sorgenti/Piano_Di_Qualifica/main.tex", "sorgenti/Piano_Di_Qualifica/sections/")
